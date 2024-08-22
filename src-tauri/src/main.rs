a90b55b4bd21a77d1d5cc12279dcd787c85f4496
akmot9 <avicocyprien@yahoo.com>
2024-08-22 09:50:12 +0200

    bydevtols

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,28 +15,10 @@ use tauri_state::SimPcapState;
 
 mod errors;
 
-use tauri::Manager;
-use tauri::GlobalShortcutManager;
-
 fn main() -> Result<(), tauri::Error>{
     tauri::Builder::default()
         // Manage our arced SystemState
         .manage( Arc::new(Mutex::new(SimPcapState::default())))
-        .setup(|app| {
-            let app_handle = app.handle(); // Get the AppHandle
-            let window = app_handle.get_window("main").unwrap();
-            
-            // Open DevTools
-            window.open_devtools();
-            
-            // Register a global shortcut
-            let mut shortcuts = app_handle.global_shortcut_manager();
-            shortcuts.register("CmdOrCtrl+Shift+I", move || {
-              window.open_devtools();
-            }).unwrap();
-      
-            Ok(())
-          })
         .invoke_handler(tauri::generate_handler![
             get_interfaces,
             start_packet_sending,
